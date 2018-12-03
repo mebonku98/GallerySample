1f8fc0feb6ce593618ac35586ee2ac72a8d16326
mebonku98 <44865625+mebonku98@users.noreply.github.com>
2018-12-03 16:30:39 +0530

    Add files via upload
    experiment with image loading

---
+++ GallerySample/GallerySample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GallerySample
+//
+//  Created by Ankita Ghosh on 03/12/18.
+//  Copyright © 2018 Ankita Ghosh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
